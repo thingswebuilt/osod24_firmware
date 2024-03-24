ec488b2c9ee97bc806500f3655d40d82640df9a5
Rob Berwick <rob.berwick@gmail.com>
2024-03-24 15:33:48 +0000

    show i2c bus scan results if bno08x not found

--- src/main.cpp
+++ src/main.cpp
@@ -67,6 +67,7 @@ int main() {
     BNO08x IMU;
     while (IMU.begin(CONFIG::BNO08X_ADDR, i2c_port0)==false) {
         printf("BNO08x not detected at default I2C address. Check wiring. Freezing\n");
+        scan_i2c_bus();
         sleep_ms(1000);
     }
     IMU.enableRotationVector();
